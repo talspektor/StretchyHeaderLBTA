0019dd4fe5a792f270b46c24e36fd822606e44c7
user140592 <user140592@eu201.local>
2018-12-26 20:19:56 +0000

    Initial Commit

---
+++ StretchyHeaderLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyHeaderLBTA
+//
+//  Created by user140592 on 12/26/18.
+//  Copyright © 2018 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
